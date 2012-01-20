e361c7e455d8dc6735d696a6a7f538b323b94d27
Rémi Duraffort <ivoire@videolan.org>
2012-01-20 12:33:52 +0100

    Check that the library is initialized prior the execution of any hooks.
    Indeed the constructor functionof the library can be called after one of the
    hook when the hooks are called from the constructor function of another binary.

--- timescaler.c
+++ timescaler.c
@@ -14,6 +14,7 @@
 # define LOCAL
 #endif
 
+LOCAL int timescaler_initialized = 0;
 LOCAL int timescaler_verbosity = 0;
 LOCAL int timescaler_scale = 0;
 LOCAL int timescaler_initial_time;
@@ -30,6 +31,18 @@ LOCAL int (*timescaler_nanosleep)(const struct timespec *req, struct timespec *r
  */
 LOCAL void __attribute__ ((constructor)) timescaler_init(void)
 {
+  /*
+    The constructor function is not always the first function to be called.
+    Indeed one of the hooks can be called by the constructor of a binary
+    intiailialized before the timescaler library.
+    In this case the hook should call the constructor and then continue to
+    execute the right code.
+  */
+  if(timescaler_initialized)
+    return;
+  timescaler_initialized = 1;
+
+  /* Fetch the configuration from the environment variables */
   const char *psz_verbosity = getenv("TIMESCALER_VERBOSITY");
   if(psz_verbosity)
     timescaler_verbosity = atoi(psz_verbosity);
@@ -38,7 +51,13 @@ LOCAL void __attribute__ ((constructor)) timescaler_init(void)
   if(psz_scale)
     timescaler_scale = atoi(psz_scale);
 
-  timescaler_real_time = dlsym(RTLD_NEXT, "time");
+
+  /* Resolv the symboles that we will need afterward */
+  timescaler_gettimeofday = dlsym(RTLD_NEXT, "gettimeofday");
+  timescaler_nanosleep    = dlsym(RTLD_NEXT, "nanosleep");
+  timescaler_real_sleep   = dlsym(RTLD_NEXT, "sleep");
+  timescaler_real_time    = dlsym(RTLD_NEXT, "time");
+
   timescaler_initial_time = timescaler_real_time(NULL);
 
   if(timescaler_verbosity > 0)
@@ -50,6 +69,9 @@ LOCAL void __attribute__ ((constructor)) timescaler_init(void)
  */
 GLOBAL time_t time(time_t* tp)
 {
+  if(!timescaler_initialized)
+    timescaler_init();
+
   time_t now = timescaler_real_time(tp);
   return timescaler_initial_time + (now - timescaler_initial_time) / timescaler_scale;
 }
@@ -57,8 +79,9 @@ GLOBAL time_t time(time_t* tp)
 
 GLOBAL int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
-  if(!timescaler_gettimeofday)
-    timescaler_gettimeofday = dlsym(RTLD_NEXT, "gettimeofday");
+  if(!timescaler_initialized)
+    timescaler_init();
+
   int return_value = timescaler_gettimeofday(tv, tz);
   tv->tv_sec = timescaler_initial_time + (tv->tv_sec - timescaler_initial_time) / timescaler_scale;
   return return_value;
@@ -70,8 +93,8 @@ GLOBAL int gettimeofday(struct timeval *tv, struct timezone *tz)
  */
 GLOBAL unsigned int sleep(unsigned int seconds)
 {
-  if(!timescaler_real_sleep)
-    timescaler_real_sleep = dlsym(RTLD_NEXT, "sleep");
+  if(!timescaler_initialized)
+    timescaler_init();
 
   unsigned int return_value = timescaler_real_sleep(seconds * timescaler_scale);
   return return_value / timescaler_scale;
@@ -79,8 +102,8 @@ GLOBAL unsigned int sleep(unsigned int seconds)
 
 GLOBAL int nanosleep(const struct timespec *req, struct timespec *rem)
 {
-  if(!timescaler_nanosleep)
-    timescaler_nanosleep = dlsym(RTLD_NEXT, "nanosleep");
+  if(!timescaler_initialized)
+    timescaler_init();
 
   struct timespec req_scale;
   //TODO: scale the tv_nsec element
